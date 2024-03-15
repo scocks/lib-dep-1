05e19fb477efe1c7b14ff571cfe765dd878426ff
Steven Cocks <ext.steven.cocks@king.com>
2024-03-15 08:26:11 +0100

    Change defaults

--- src/test/java/org/libDep1/MainTest.java
+++ src/test/java/org/libDep1/MainTest.java
@@ -18,7 +18,7 @@ class MainTest {
     @Test
     void testGreet() {
         Library library = new Library();
-        String result = library.greet("Steve");
-        assertEquals("Hello, Steve!", result);
+        String result = library.greet("lib-dep-1");
+        assertEquals("Hello, lib-dep-1!", result);
     }
 }
